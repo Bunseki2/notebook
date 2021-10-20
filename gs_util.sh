c050e87d42f20e2e050bbd3b5b9f36dd24683313
Bunseki2 <32596179+Bunseki2@users.noreply.github.com>
2021-10-20 23:02:16 +0000

    Add files via upload

---
+++ gs_util.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v1/5320835_MARK4_32-1_80A_v1.0.dcd  .
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v1/5320835_MARK4_32-1_80A_v1.prmtop  .
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v1/5320835_MARK4_32-1_80A_v1.log  .
+
+
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v2/5320835_MARK4_32-1_80A_v2.0.dcd  .
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v2/5320835_MARK4_32-1_80A_v2.prmtop  .
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v2/5320835_MARK4_32-1_80A_v2.log  .
+
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v3/5320835_MARK4_32-1_80A_v3.0.dcd  .
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v3/5320835_MARK4_32-1_80A_v3.prmtop  .
+gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/5320835_MARK4_32-1_80A_v3/5320835_MARK4_32-1_80A_v3.log  .
+
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v1/MARK4_1_80A_v1.0.dcd .
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v2/MARK4_1_80A_v2.0.dcd .
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v3/MARK4_1_80A_v3.0.dcd .
+
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v1/MARK4_1_80A_v1.log .
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v2/MARK4_1_80A_v2.log .
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v3/MARK4_1_80A_v3.log .
+
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v1/MARK4_1_80A_v1.prmtop .
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v2/MARK4_1_80A_v2.prmtop .
+#gsutil cp gs://biostasis002/sims/quercetin_MARK4_003/OUTPUTS/MARK4_1_80A_v3/MARK4_1_80A_v3.prmtop .
